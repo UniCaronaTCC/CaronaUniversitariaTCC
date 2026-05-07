0dc6c9c2064660b236614f4527ad3168cecfbee1
Henrique Ortiz Martins <henriqueortizmartins2022@gmail.com>
2026-05-07 20:38:59 -0300

    cadastro atualizado

--- frontend/lib/auth/cadastro.dart
+++ frontend/lib/auth/cadastro.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
+import '../config/app_colors.dart';
 
-class CadastroTela extends StatelessWidget {
-  const CadastroTela({super.key});
+class CadastroTela extends StatelessWidget { //cria a tela de cadastro
+  const CadastroTela({super.key}); //obrigatorio para construir a tela
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cadastro'),
+  @override //substitui/sobrescreve o metodo da classe pai
+  Widget build(BuildContext context) { // monta a tela toda, tudo visual fica ai
+    return Scaffold( //base da tela, quase smp tem que ter organiza o resto
+      appBar: AppBar(  //cria a barra do topo da tela
+        title: const Text('Cadastro'), // texto que aparece no appBar
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(24),
-        child: Column(
-          children: [
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Nome',
-                border: OutlineInputBorder(),
+
+      body: Padding( //BODY: o que vai ter na tela principal // PADDING cria o espaçamento interno
+        padding: const EdgeInsets.all(24), // vai colocar 24px de espaco em todos os lados
+        child: Column( //pra organizar os intens em coluna (um embaixo do outro)
+          mainAxisAlignment: MainAxisAlignment.center, // para centralizar os itens verticalmente, se nao ia ficar tudo no topo
+          children: [ // O children é a lista de coisas que vão aparecer dentro da coluna
+            const TextField( // Cria um campo para o usuário digitar
+              decoration: InputDecoration( //para costumizar o campo
+                labelText: 'Nome', // o texto que vai ficar no campo
+                enabledBorder: OutlineInputBorder( //cria a borda em volta do campo
+                  borderSide: BorderSide(color: AppColors.primary), //para definir a cor da borda, aqui esta usando a predefinida do app
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: AppColors.primary, width: 2),
+                ),
               ),
             ),
 
@@ -25,7 +33,12 @@ class CadastroTela extends StatelessWidget {
             const TextField(
               decoration: InputDecoration(
                 labelText: 'E-mail',
-                border: OutlineInputBorder(),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: AppColors.primary),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: AppColors.primary, width: 2),
+                ),
               ),
             ),
 
@@ -35,7 +48,12 @@ class CadastroTela extends StatelessWidget {
               obscureText: true,
               decoration: InputDecoration(
                 labelText: 'Senha',
-                border: OutlineInputBorder(),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: AppColors.primary),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: AppColors.primary, width: 2),
+                ),
               ),
             ),
 
@@ -45,7 +63,7 @@ class CadastroTela extends StatelessWidget {
               width: double.infinity,
               child: ElevatedButton(
                 onPressed: () {
-                  print('Cadastrar clicado');
+                  print('Cadastro clicado');
                 },
                 child: const Text('Cadastrar'),
               ),
