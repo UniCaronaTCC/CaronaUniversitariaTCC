3078d0f311115ff54e2c27149e4ad01d24c66c89
Henrique Ortiz Martins <henriqueortizmartins2022@gmail.com>
2026-05-12 20:32:19 -0300

    cadastro cath
    # Conflicts:
    #	frontend/lib/auth/cadastro.dart

--- frontend/lib/auth/cadastro.dart
+++ frontend/lib/auth/cadastro.dart
@@ -97,7 +97,7 @@ class _CadastroTelaState extends State<CadastroTela> {
           resultado['sucesso'] == true
 
           // mensagem de sucesso
-              ? resultado['dados']['mensagem']
+              ? resultado['dados']['message']
               ?? 'Cadastro realizado com sucesso'
 
           // mensagem de erro
@@ -105,9 +105,6 @@ class _CadastroTelaState extends State<CadastroTela> {
         ),
       ),
     );
-    if (resultado['sucesso'] == true) { // Se o cadastro deu certo
-      Navigator.pop(context); // Volta para a tela anterior, que é o login
-    }
   }
 
   @override
