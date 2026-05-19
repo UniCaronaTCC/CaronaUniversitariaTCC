7bfdaa078101de5f8f3108f454a6784d5f12697a
João Pedro <joaopedrofm@outlook.com>
2026-05-18 22:18:51 -0300

    coloquei a fonte inter e comecei a tela inicial

--- frontend/lib/config/app_theme.dart
+++ frontend/lib/config/app_theme.dart
@@ -3,6 +3,7 @@ import 'app_colors.dart'; // importa o arquivo onde estão as cores principais d
 
 class AppTheme { // cria a classe responsável por guardar o tema visual do app
   static ThemeData lightTheme = ThemeData( // cria o tema claro do aplicativo
+    fontFamily: 'Inter', // Coloca a fonte Inter
     scaffoldBackgroundColor: AppColors.background, // define a cor de fundo padrão das telas
     colorScheme: ColorScheme.fromSeed( // cria um conjunto de cores baseado em uma cor principal
       seedColor: AppColors.primary, // usa a cor principal do app como base do tema
