ef5110c12b273b177eb0d6344e4058bc401a947b
Henrique Ortiz Martins <henriqueortizmartins2022@gmail.com>
2026-08-04 21:15:07 -0300

    at

--- frontend/lib/config/api_config.dart
+++ frontend/lib/config/api_config.dart
@@ -5,6 +5,8 @@ class ApiConfig {
   // Em outro ambiente, use --dart-define=API_BASE_URL=http://endereco:porta.
   static const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://10.0.2.2:3000',
+    defaultValue: 'http://192.168.137.1',
+    // 192.168.137.1 para usar no celular
+    //http://10.0.2.2:3000 para usar no emulador
   );
 }
